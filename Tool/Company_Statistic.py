6dae4b6ae7d40cde76ef773eff1e848f9e5d0110
Chengtianlun <xyzlun@163.com>
2019-11-02 21:01:24 +0800

    creat statistic func

---
+++ Tool/Company_Statistic.py
@@ -0,0 +1,66 @@
+# _*_ coding: utf-8 _*_
+__filename__ = 'Company_Statistic'
+__author__ = 'chengtianlun'
+__date__ = '2019/11/2 12:16'
+
+import os
+import xlwt
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from collections import Counter
+
+
+from Tool import Data_Creator,Map_Labeler
+from Algorithm import kMeans
+from Models import *
+from TestModel import *
+
+def CompanyStatistic(companys,industryLabelDict,k,filename):
+    workbook = xlwt.Workbook(encoding='utf-8')  # 新建工作簿
+    sheet1 = workbook.add_sheet('sheet1')  # 新建sheet
+
+    # 填写行标题
+    for i in range(0,k,2):
+        sheet1.write(0,i,'Cluster '+str(i))
+        sheet1.write(0,i + 1,'占比')
+
+    for i in range(k):
+        cluster_label = companys.with_entities(CompanyTest.industry_label).filter(CompanyTest.clusterID == i)
+        total = cluster_label.count()
+        temp_list = []
+        for item in cluster_label:
+            temp_list.append(industryLabelDict[item[0]])
+        ElemCounter = Counter(temp_list)
+        for j,item in enumerate(ElemCounter):
+            sheet1.write(j+1,i*2,item)
+            # sheet1.write(j+1,i*2+1,ElemCounter[item])  # 个数
+            sheet1.write(j+1,i*2+1,'{:.2f}%'.format(ElemCounter[item]/total*100))  # 百分比
+
+    workbook.save('./result/'+filename+'.xls')   #保存
+    print('company statistic sheet saved')
+
+
+
+
+
+
+if __name__=='__main__':
+    # 创建连接引擎
+    host = 'localhost'
+    port = 3306
+    username = 'root'
+    password = 'password'
+    db = 'qcc_data'
+    connect_str = 'mysql+mysqlconnector://{}:{}@{}:{}/{}'.format(username, password, host, port,db)
+
+    engine = create_engine(connect_str, echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    #companysQuery = session.query(CompanyTest).filter(CompanyTest.clusterID==1)  # 读取所有公司信息
+    companysQuery = session.query(CompanyTest)
+    categoryInfoQuery = session.query(CategoryInfoTest)
+    industry_labelQuery = session.query(IndustryLabelTest)
+    IndustryLabelDict = Data_Creator.CategoryDict(categoryInfoQuery)  # 键为ID，值为行业名
+
+    CompanyStatistic(companysQuery,IndustryLabelDict,18,'test')
